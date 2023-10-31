5941da63be6058b662c606017bf6c4722023bb07
Aryan Soni <arpitsoni811@gmail.com>
2023-11-01 02:51:15 +0530

    making login components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,79 @@
+import logo from "./assets/100xlogo.svg";
+
+//buttons
+function Buttons(props) {
+  const base =
+    "px-6 py-2 rounded-full font-bold items-center disabled:opacity-50 disabled:pointer-events-none";
+
+  const variant = {
+    blue: "text-neutral-50 bg-twitter-blue hover:bg-twitter-blue-2",
+    white: " text-neutral-1000 bg-neutral-50 hover:bg-neutral-200",
+    outline:
+      "text-twitter-blue bg-neutral-1000 hover:bg-neutral-900 border-blue-wash border-2",
+  };
+  const sizes = {
+    sm: "",
+    md: "w-20",
+    lg: "w-full",
+  };
+
+  const classes = `${base} ${variant[props.variant]} ${sizes[props.size]}`;
+
+  return <button className={classes}>{props.text}</button>;
+}
+//Header
+function Header() {
+  return (
+    <img
+      className="object-contain my-3 w-14 h-4 sm:w-36 sm:h-12 md:w-72 md:h-24 lg:w-[600px] lg:h-52"
+      src={logo}
+      alt="100xlogo"
+    />
+  );
+}
+
+//main content
+function MainContent() {
+  return (
+    <div className="flex flex-col pl-7 min-h-screen">
+      <div className="m-auto">
+        <h1 className="text-3.5xl md:text-5xl mb-3 font-extrabold ">
+          Happening Now
+        </h1>
+        <h3 className="text-base md:text-2.5xl pb-10 font-bold">Join today.</h3>
+        <div className="pr-7">
+          <a href="../src/step1.html">
+            <Buttons
+              variant="white"
+              size="lg"
+              text="Create account"
+              // disabled={true}
+            />
+          </a>
+          <div className="flex items-center justify-center py-10 gap-1">
+            <hr width="150.5px" />
+            or
+            <hr width="150.5px" />
+          </div>
+          <h2 className="text-xl font-medium pb-5">
+            Already have an account ?
+          </h2>
+
+          <Buttons variant="outline" size="lg" text="Sign-up" />
+        </div>
+      </div>
+    </div>
+  );
+}
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="font-inter bg-neutral-1000 text-neutral-50 h-screen w-screen justify-center items-start lg:items-center flex px-7">
+        <div className="flex items-center md:gap-6 lg:gap-25 flex-col md:flex-row">
+          <Header />
+          <MainContent />
+        </div>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,46 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", { lineHeight: "1.188em" }],
+        // when finishing add sans-serif after inter
+      },
+      colors: {
+        "twitter-blue": "#1D9BF0",
+        "twitter-blue-2": "#1871CA",
+        "blue-wash": "#75BEEF33",
+        "button-stroke": "#1D9BF03D",
+        "searchbar-fill": "#212327",
+        "card-fill": "#16181C",
+        "green-success": "#00BE74",
+        "red-error": "#F4245E",
+        "red-like": "rgb(239 68 68)",
+        "neutral-50": "#f9f9f9",
+        "neutral-100": "#f4f4f4",
+        "neutral-200": "#e4e4e4",
+        "neutral-300": "#d3d3d3",
+        "neutral-400": "#a2a2a2",
+        "neutral-500": "#737373",
+        "neutral-600": "#525252",
+        "neutral-700": "#404040",
+        "neutral-800": "#262626",
+        "neutral-900": "#171717",
+        "neutral-950": "#0a0a0a",
+        "neutral-1000": "#000000",
+      },
+      spacing: {
+        25: "6.25rem",
+      },
+      fontSize: {
+        "3.5xl": ["1.938rem", { lineHeight: "2.5rem" }],
+        "2.5xl": ["1.625rem", { lineHeight: "2rem" }],
+        lg3: ["1.188rem", { lineHeight: "1.4rem" }],
+        lg2: ["1.063rem", { lineHeight: "1.3rem" }],
+        sm2: ["0.938rem", { lineHeight: "1.2rem" }],
+        xxs: ["0.625rem", { lineHeight: "0.75rem" }],
+      },
+    },
   },
   plugins: [],
 };
